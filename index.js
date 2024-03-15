b5b55c2f5388a5acd30367b179539d40659053c1
DKM1111 <dinesh.kumar.fullstack@gmail.com>
2024-03-15 15:03:54 +0530

    deploye

--- index.js
+++ index.js
@@ -133,6 +133,6 @@ app.delete('/users/:id', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log(`server running at http://localhost:3000`);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
